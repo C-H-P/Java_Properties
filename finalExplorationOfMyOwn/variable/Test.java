d6af51d8de5130a5a0982c0915c2a6fa3856d7c3
C-H-P <rijiaohe@gmail.com>
2023-04-14 23:53:09 -0600

    modified final exploration

--- finalExplorationOfMyOwn/variable/Test.java
+++ finalExplorationOfMyOwn/variable/Test.java
@@ -1,7 +1,8 @@
 public class Test{
 	public static final int x=1;
 	public static void main(String[] args){
-		int x=2;
+		//final variable x cannot be modified 
+		//This will return a compilation error
 		System.out.println(x++);
 	}
 }
