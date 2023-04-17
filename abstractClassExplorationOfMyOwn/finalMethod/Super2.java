ef1958b64f2cce52c1882924de2b8e2f33df18d2
C-H-P <rijiaohe@gmail.com>
2023-04-17 14:15:05 -0600

    added final and static keyword in abstract classes

---
+++ abstractClassExplorationOfMyOwn/finalMethod/Super2.java
@@ -0,0 +1,3 @@
+public abstract class Super{
+	public abstract final void foo();
+}
---
+++ abstractClassExplorationOfMyOwn/staticMethod/Super.java
@@ -0,0 +1,5 @@
+public abstract class Super{
+	public static void foo(){
+		System.out.println("foo called in the abstract class");
+	}
+}
